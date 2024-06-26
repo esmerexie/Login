dbe294eee5cfe225f069d2df019b01475129a9df
Rexie <esmeraldarexie@gmail.com>
2024-06-26 16:19:25 -0700

    pushing some minor changes

--- README.md
+++ README.md
@@ -4,3 +4,7 @@ An app to replicate a simple login that hashes users information.
 ## 25 June 2024
 initilized the project to include the dependecies included in the Spring Boot Package.
 can be found using this link https://start.spring.io/
+
+
+## 26 June 2024
+Started working on the logic for the backened to connect the application to the local database pgAdmin
--- demo/src/main/java/com/models/SiteUser.java
+++ demo/src/main/java/com/models/SiteUser.java
@@ -1,7 +1,22 @@
 package main.java.com.models;
 
-import javax.*;
+import javax.persistence.*;
 
+@Entity
 public class SiteUser {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.Identity)
+
+    private Long id;
+    private String username;
+    private String password;
+
+    pulic SiteUser(){
+    }
+
+    public SiteUser(String username, String password){
+        this.username = username;
+        this.password = password;
+    }
 }
