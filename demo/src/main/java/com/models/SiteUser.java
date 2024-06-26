39e927185702feab10df6f66a49e66e1ef366783
Rexie <esmeraldarexie@gmail.com>
2024-06-26 16:25:53 -0700

    added the getter/setters to the User model

--- demo/src/main/java/com/models/SiteUser.java
+++ demo/src/main/java/com/models/SiteUser.java
@@ -19,4 +19,28 @@ public class SiteUser {
         this.username = username;
         this.password = password;
     }
+
+    public Long getId(){
+        return id;
+    }
+
+    public void setId(Long id){
+        this.id = id;
+    }
+
+    public String getUsername(){
+        return username;
+    }
+
+    public void setUsername(String username){
+        this.username = username;
+    }
+
+    public String getPassword(){
+        return password;
+    }
+
+    public void setPassword(String password){
+        this.password = password;
+    }
 }
